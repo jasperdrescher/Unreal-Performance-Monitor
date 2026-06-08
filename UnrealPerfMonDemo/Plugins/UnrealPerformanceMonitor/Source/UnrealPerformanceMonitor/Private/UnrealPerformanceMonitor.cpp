44eec7503ae65166660fc832b929cedd931b7215
Jasper Drescher <jasperdre@gmail.com>
2026-06-08 17:54:24 +0900

    Add plugin boilerplate

---
+++ UnrealPerfMonDemo/Plugins/UnrealPerformanceMonitor/Source/UnrealPerformanceMonitor/Private/UnrealPerformanceMonitor.cpp
@@ -0,0 +1,22 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealPerformanceMonitor.h"
+
+#define LOCTEXT_NAMESPACE "FUnrealPerformanceMonitorModule"
+
+void FUnrealPerformanceMonitorModule::StartupModule()
+{
+	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+	
+}
+
+void FUnrealPerformanceMonitorModule::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+	
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FUnrealPerformanceMonitorModule, UnrealPerformanceMonitor)
---
+++ UnrealPerfMonDemo/Plugins/UnrealPerformanceMonitor/Source/UnrealPerformanceMonitor/Private/UnrealPerformanceMonitorBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealPerformanceMonitorBPLibrary.h"
+#include "UnrealPerformanceMonitor.h"
+
+UUnrealPerformanceMonitorBPLibrary::UUnrealPerformanceMonitorBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UUnrealPerformanceMonitorBPLibrary::UnrealPerformanceMonitorSampleFunction(float Param)
+{
+	return -1;
+}
+
